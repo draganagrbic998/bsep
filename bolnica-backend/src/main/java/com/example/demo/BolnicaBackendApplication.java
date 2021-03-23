1b789b94462d6c0727d5abf99c0a3d32a8c24619
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-23 12:20:11 +0100

    backend za bolnicicu

---
+++ bolnica-backend/src/main/java/com/example/demo/BolnicaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolnicaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolnicaBackendApplication.class, args);
+	}
+
+}
