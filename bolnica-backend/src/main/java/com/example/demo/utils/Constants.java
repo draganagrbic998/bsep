b78501e0d697a11eeebf2c726b7f2658777d57d4
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-03-26 01:20:52 +0100

    ssl na frontu bolnice

--- bolnica-backend/src/main/java/com/example/demo/utils/Constants.java
+++ bolnica-backend/src/main/java/com/example/demo/utils/Constants.java
@@ -2,8 +2,8 @@ package com.example.demo.utils;
 
 public class Constants {
 
-	public static final String BACKEND = "http://localhost:8081";	
-	public static final String FRONTEND = "http://localhost:4201";	
+	public static final String BACKEND = "https://localhost:8081";	
+	public static final String FRONTEND = "https://localhost:4201";	
 	public static final String ALARM_RULES = "alarm-rules";
 
 	public static final String ENABLE_HEADER = "Access-Control-Expose-Headers";
