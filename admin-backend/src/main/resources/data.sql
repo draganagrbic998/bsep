f856f46e427909c620742ca143b29d92865b6745
draganagrbic998 <dragana.grbic.98@uns.ac.rs>
2021-04-22 15:16:30 +0200

    sitnisic mali

--- admin-backend/src/main/resources/data.sql
+++ admin-backend/src/main/resources/data.sql
@@ -20,14 +20,3 @@ country, email, template, start_date, end_date, revoked)
 values (2, 1, 'device1', 'asd', 'asd', 'asd', 
 'AS', 'draganaasd@gmail.com', 'SUB_CA', '2021-04-07', '2031-04-07', false);
 
-insert into certificate_info (
-issuer_id, extensions_id, alias, common_name, organization, organization_unit, 
-country, email, template, start_date, end_date, revoked)
-values (1, 1, 'hospital2', 'asd', 'asd', 'asd', 
-'AS', 'draganaasd@gmail.com', 'SUB_CA', '2021-04-07', '2031-04-07', false);
-
-insert into certificate_info (
-issuer_id, extensions_id, alias, common_name, organization, organization_unit, 
-country, email, template, start_date, end_date, revoked)
-values (4, 1, 'device2', 'asd', 'asd', 'asd', 
-'AS', 'draganaasd@gmail.com', 'SUB_CA', '2021-04-07', '2031-04-07', false);
